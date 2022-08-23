250d275f69f1ebfb1331c0c5a301ec5d65427e4a
SayedMajid <sydmajid04@gmail.com>
2022-08-24 01:11:48 +0530

    Categories

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import Categories from "./Components/Categories";
 
 function App() {
   return (
     <div>
-      
+      <Categories />
     </div>
   );
 }
