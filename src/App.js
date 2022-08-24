93860baa89e0e32433c82ab9460866d36adf54fb
SayedMajid <sydmajid04@gmail.com>
2022-08-24 16:05:58 +0530

    Reviews Carousel : Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import CarouselReview from "./Components/CarouselReview";
 import Categories from "./Components/Categories";
 
 function App() {
   return (
     <div>
       <Categories />
+      <CarouselReview />
     </div>
   );
 }
