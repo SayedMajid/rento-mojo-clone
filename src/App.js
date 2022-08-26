a72e18ad398603aeadde30caae4b4b1fae91c93c
Rahim Ansari <rahimansari4560@gmail.com>
2022-08-26 06:50:12 +0530

    Login Functionalities Done

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-<<<<<<< HEAD
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { RahiHome } from './Components/RahiHome';
 import RahiRoutes from './Components/RahiRoutes';
 import Wfh from './Components/Wfh';
 import WfhDetails from './Components/WfhDetails';
-=======
 import "./App.css";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 import AllRoutes from "./Pages/AllRoutes";
->>>>>>> d2c9ddeddf14ab7e38877c27dd703cb43a880e1c
-
+import { Container } from '@chakra-ui/react';
+import Login from './Components/Login';
 function App() {
   return (
     <div>
-<<<<<<< HEAD
+
      
     
         {/* <RahiRoutes /> */}
-=======
-      <Navbar />
+
+    {/* <Navbar />
       <AllRoutes />
-      <Footer />
->>>>>>> d2c9ddeddf14ab7e38877c27dd703cb43a880e1c
+      <Footer />  */}
+      <Login />
     </div>
   );
 }
