11008c4bf373b4bf87072dd4032eefdf45b926f8
Priyanshu Anany <ananypriyanshu@gmail.com>
2022-08-26 20:05:22 +0530

    Bug fixed

--- src/App.js
+++ src/App.js
@@ -3,23 +3,6 @@ import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 import AllRoutes from "./Pages/AllRoutes";
 
-import { Container } from '@chakra-ui/react';
-import Login from './Components/Login';
-import Cart from './Components/Cart';
-function App() {
-  return (
-    <div>
-
-     
-    
-        {/* <RahiRoutes /> */}
-        {/* <Cart /> */}
-
-    <Navbar />
-      <AllRoutes />
-      <Footer /> 
-      {/* <Login /> */}
-
 
 
 function App() {
